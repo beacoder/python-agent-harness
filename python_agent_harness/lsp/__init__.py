c012f0641a83dfa1a2c0151c1451ac3a0e5dbf9c
陈虎明 <chenhuming@gmail.com>
2026-09-10 10:39:33 +0800

    Update __init__.py

--- python_agent_harness/lsp/__init__.py
+++ python_agent_harness/lsp/__init__.py
@@ -1,6 +1,14 @@
 """Minimal built-in LSP client used by python-agent-harness."""
 
 from .client import LSPClient, LSPError
+from .config import LSPConfig, LSPServerConfig
 from .manager import get_client, shutdown_all
 
-__all__ = ["LSPClient", "LSPError", "get_client", "shutdown_all"]
+__all__ = [
+    "LSPClient",
+    "LSPConfig",
+    "LSPError",
+    "LSPServerConfig",
+    "get_client",
+    "shutdown_all",
+]
