8d93a3261e915eb0e66c05383a6fc7564a866b62
陈虎明 <chenhuming@gmail.com>
2026-09-17 10:28:02 +0800

    Update glob_win.py

--- python_agent_harness/tools/glob_win.py
+++ python_agent_harness/tools/glob_win.py
@@ -73,6 +73,7 @@ class GlobWindows(GlobTool):
             return "Error: pattern must not be empty"
         path = args.get("path")
         if path:
+            path = os.path.expanduser(path)
             if not (os.path.isdir(path) and os.access(path, os.R_OK)):
                 return f"Error: path {path} is not readable"
         else:
